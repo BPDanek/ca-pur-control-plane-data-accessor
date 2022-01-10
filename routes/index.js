3b262e955fc4075808c9aa7d343303d1645ea4f0
BPDanek <danekbenjamin@gmail.com>
2022-01-09 22:05:17 -0800

    integrate yearly table & clean up

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
-var db_config = require('../secrets');
 var express = require('express');
 var router = express.Router();
-var pgp = require('pg-promise')(/* options */)
-var db = pgp(db_config)
 const cors = require("cors");
 
 var queryPURTable = require('../server-control-plane/query-pur-db')
@@ -25,6 +22,7 @@ router.get('/test', cors(), function(req, res) {
 /*
  * format of query is ?counties=["count1_id","count2_id", "count3_id",...] where countyX_id is the id for a CA county
  * responds with ["county1_count", "county2_count", "county3_count", ...] where countyX_count is the number of used pesticides for a county
+ * ex: `http://localhost:3001/query-pur-db?counties=[10,11,12]`
  */
 router.get('/query-pur-db', cors(), function(req, res, next) {
 
@@ -37,7 +35,7 @@ router.get('/query-pur-db', cors(), function(req, res, next) {
     }
 
     const pesticide_count_promises = [];
-    queryPURTable(pesticide_count_promises, counties)
+    queryPURTable(pesticide_count_promises, counties, false)
 
     // when all requests resolve then send them over
     // the format is [...counts, maxCount] <- maxCount is used to provide scaling for each of the count values
@@ -47,6 +45,54 @@ router.get('/query-pur-db', cors(), function(req, res, next) {
             res.status(200).send(count) // return to sender
         })
         .catch((error) => {console.log('Query Promise Errors:', error)})
+})
+
+/*
+    An updated route that will query multiple databases depending on route parameters. Built to support different tables
+    including ca_pur_reduced and ca_pur_yearly_reduced
+    {hosturl}/pur?counties={str[]}&table={int}
+    ex:
+    .../pur?counties=["county_id", "county_id", ...]&table=0
+
+
+    We are returning table 1 in all cases, except where they specifically request the non-yearly table
+    table 0: ca_reduced_udc
+    table 1: ca_yearly_reduced_udc (latest)
+ */
+router.get('/pur', cors(), function(req, res, next) {
+
+    console.log("counties", req.query.counties)
+    console.log("table", req.query.table)
+    console.log("query", req.query)
+
+    try {
+        if (req.query.counties === undefined || req.query.table === undefined) {
+            throw "Counties query parameter or table parameter in route `/pur` undefined."
+        }
+
+        // grab the query parameter and clean it up for using in SQL in the future do more stringent cleaning & validation
+        let counties = req.query.counties.replace('[', '').replace(']', '').split(',', )
+        if (counties.length > 58) {
+            console.log("invalid query")
+            res.send("Invalid query parameters. ")
+            return
+        }
+
+        const pesticide_count_promises = [];
+        queryPURTable(pesticide_count_promises, counties, req.query.table !== "1" ? false : true)
+
+        // when all requests resolve then send them over
+        // the format is [...counts, maxCount] <- maxCount is used to provide scaling for each of the count values
+        Promise.all(pesticide_count_promises)
+            .then((count) => {
+                console.log("Counts:", count.slice(0, -1), "Max:", count.slice(-1))
+                res.status(200).send(count) // return to sender
+            })
+            .catch((error) => {console.log('Query Promise Errors:', error)})
+
+    } catch (e) {
+        res.send("Invalid query parameters. ")
+    }
 })
 
 module.exports = router;
--- server-control-plane/query-pur-db.js
+++ server-control-plane/query-pur-db.js
@@ -1,11 +1,15 @@
+var db_config = require('../secrets');
+var pgp = require('pg-promise')(/* options */)
+var db = pgp(db_config)
+
 function getPesticideCounts(pesticide_count_promises, counties) {
     // query db using pgPromise for async db access. http://vitaly-t.github.io/pg-promise/Database.html#one
-    for (var row_index = 0; row_index < counties.length; row_index++) {
+    for (let row_index = 0; row_index < counties.length; row_index++) {
 
         // build query for route parameters
         const query = `SELECT pesticide_count FROM ca_reduced_udc WHERE county_cd = '${counties[row_index]}';`
 
-        console.log("query: ", query)
+        console.log("non-yearly query: ", query)
 
         // unpack count and push to an array of async requests/Promises
         pesticide_count_promises.push(
@@ -21,20 +25,69 @@ function getPesticideCounts(pesticide_count_promises, counties) {
     }
 }
 
+function getYearlyPesticideCounts(pesticide_count_promises, counties) {
+    // query db using pgPromise for async db access. http://vitaly-t.github.io/pg-promise/Database.html#one
+    for (let row_index = 0; row_index < counties.length; row_index++) {
+
+        // build query for route parameters
+        const query = `SELECT count_2012, count_2013, count_2014, count_2015, count_2016, count_2017, count_2018 FROM ca_yearly_reduced_udc WHERE county_cd = '${counties[row_index]}';`
+
+        console.log("yearly query: ", query)
+
+        // unpack count and push to an array of async requests/Promises
+        pesticide_count_promises.push(
+            db.oneOrNone(query)
+                .then(queryResponse => Object.values(queryResponse))
+                .then(queryResult => {
+                    console.log(query, queryResult)
+                    return queryResult
+                })
+                .catch(() => {
+                    console.log("failure on", query)
+                    return 0
+                })
+        )
+    }
+}
+
+/*
+    returns highest reported pesticide count in table. Since this table only contains a recent, year it is the count of
+    the pesticides for the county which has the most pesticides used.
+    todo: make this more elegant
+    precomputed to save time.
+    'SELECT MAX(pesticide_count) FROM ca_reduced_udc;'
+ */
 function getMaxPesticideCount(pesticide_count_promises) {
-    const maxPesticideCountQuery = 'SELECT MAX(pesticide_count) FROM ca_reduced_udc;'
-    pesticide_count_promises.push(
-        db.oneOrNone(maxPesticideCountQuery)
-            .then((data) => { return data.max })
-            .catch(() => {
-                console.log('Using default max pesticide count. Query failed: ', maxPesticideCountQuery)
-                return 579034
-            } )
-    )
+    pesticide_count_promises.push(579034)}
+
+/*
+    returns highest reported pesticide count in table across all yearly pesticide usage reports (pur)
+
+    todo: make this more elegant
+    precomputed to save time
+    SELECT MAX(count_2012) FROM ca_yearly_reduced_udc; --> 437571
+    SELECT MAX(count_2013) FROM ca_yearly_reduced_udc; --> 491882
+    SELECT MAX(count_2014) FROM ca_yearly_reduced_udc; --> 532544
+    SELECT MAX(count_2015) FROM ca_yearly_reduced_udc; --> 535728
+    SELECT MAX(count_2016) FROM ca_yearly_reduced_udc; --> 573328
+    SELECT MAX(count_2017) FROM ca_yearly_reduced_udc; --> 549742
+    SELECT MAX(count_2018) FROM ca_yearly_reduced_udc; --> 579034
+
+ */
+function getMaxYearlyPesticideCount(pesticide_count_promises) {
+    pesticide_count_promises.push(579034)
 }
 
 // modify pesticide_count_promises in place
-function queryPURTable(pesticide_count_promises, counties) {
-    getPesticideCounts(pesticide_count_promises, counties)
-    getMaxPesticideCount(pesticide_count_promises)
+function queryPURTable(pesticide_count_promises, counties, yearly) {
+
+    if (yearly) {
+        getYearlyPesticideCounts(pesticide_count_promises, counties)
+        getMaxYearlyPesticideCount(pesticide_count_promises)
+    } else {
+        getPesticideCounts(pesticide_count_promises, counties)
+        getMaxPesticideCount(pesticide_count_promises)
+    }
 }
+
+module.exports = queryPURTable
